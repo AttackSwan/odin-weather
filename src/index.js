bf6ea86cd9e03c999445291706b1215553cb4f00
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-07-28 11:18:01 +1000

    Process forecast data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
 import weatherManager from "./modules/weather";
 
+console.log("Index.js");
 weatherManager();
--- src/modules/weather.js
+++ src/modules/weather.js
@@ -1,12 +1,14 @@
-const testLocation = "Ballarat, Victoria";
+const defaultLocation = "Melbourne, Australia";
 
 const weatherManager = () => {
-	let weatherData;
+	const forecastData = [];
+	let dailyData;
 
 	class Weather {
 		constructor(
 			condition,
 			country,
+			daily_chance_of_rain,
 			feelsLikeC,
 			feelsLikeF,
 			humidity,
@@ -20,6 +22,7 @@ const weatherManager = () => {
 		) {
 			this.condition = condition;
 			this.country = country;
+			this.daily_chance_of_rain = daily_chance_of_rain;
 			this.feelsLikeC = feelsLikeC;
 			this.feelsLikeF = feelsLikeF;
 			this.humidity = humidity;
@@ -33,12 +36,25 @@ const weatherManager = () => {
 		}
 	}
 
+	class Forecast {
+		constructor(day, temp_highC, temp_highF, temp_lowC, temp_lowF, icon) {
+			this.day = day;
+			this.temp_highC = temp_highC;
+			this.temp_highF = temp_highF;
+			this.temp_lowC = temp_lowC;
+			this.temp_lowF = temp_lowF;
+			this.icon = icon;
+		}
+	}
+
 	async function fetchAndProcessWeather(location) {
 		try {
 			const fetchedData = await fetchWeather(location);
 			if (fetchedData) {
-				weatherData = processData(fetchedData);
-				console.log(weatherData);
+				console.log("Fetched data: ", fetchedData);
+				dailyData = processDailyData(fetchedData);
+				console.log(dailyData);
+				console.log(forecastData);
 			} else {
 				console.log("Error: No weather data to process.");
 			}
@@ -47,14 +63,14 @@ const weatherManager = () => {
 		}
 	}
 
-	async function fetchWeather(location = testLocation) {
+	async function fetchWeather(location = defaultLocation) {
 		const apiKey = API_KEY;
 		let apiURL = "";
 
 		if (location) {
-			apiURL = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=yes`;
+			apiURL = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3&aqi=yes&alerts=no`;
 		} else {
-			apiURL = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${testLocation}&aqi=yes`;
+			apiURL = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${defaultLocation}&days=3&aqi=yes&alerts=no`;
 		}
 
 		const apiResponse = await fetch(apiURL);
@@ -62,11 +78,12 @@ const weatherManager = () => {
 		return data;
 	}
 
-	function processData(data) {
+	function processDailyData(data) {
 		if (!data) {
 			console.log("error: no data to process");
 			return;
 		} else {
+			// Process data for current day:
 			// Deconstruct data object into desired parameters
 			const {
 				current: {
@@ -82,9 +99,13 @@ const weatherManager = () => {
 				location: { country, name, localtime },
 			} = data;
 
-			const processedData = new Weather(
+			const daily_chance_of_rain =
+				data.forecast.forecastday[0].day.daily_chance_of_rain;
+
+			const current_day_data = new Weather(
 				condition,
 				country,
+				daily_chance_of_rain,
 				feelslike_c,
 				feelslike_f,
 				humidity,
@@ -97,7 +118,29 @@ const weatherManager = () => {
 				wind_mph
 			);
 
-			return processedData;
+			// Process forecast data
+			const { forecast } = data;
+
+			forecast.forecastday.forEach((day) => {
+				const forecastDate = day.date;
+				const maxtempC = day.day.maxtemp_c;
+				const maxtempF = day.day.maxtemp_f;
+				const mintempC = day.day.mintemp_c;
+				const mintempF = day.day.mintemp_f;
+				const dailyIcon = day.day.condition.icon;
+
+				const dailyForecastData = new Forecast(
+					forecastDate,
+					maxtempC,
+					maxtempF,
+					mintempC,
+					mintempF,
+					dailyIcon
+				);
+				forecastData.push(dailyForecastData);
+			});
+
+			return current_day_data;
 		}
 	}
 
